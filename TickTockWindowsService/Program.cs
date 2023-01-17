93a9003ca6f7191eebab789debc294c33e58a177
Msluka <msluka.info@gmail.com>
2023-01-17 23:00:08 +0100

    Service app is ready to deploy

--- TickTockWindowsService/Program.cs
+++ TickTockWindowsService/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Topshelf;
 
 namespace TickTockWindowsService
 {
@@ -10,6 +7,24 @@ namespace TickTockWindowsService
     {
         static void Main(string[] args)
         {
+            var exitCode = HostFactory.Run(x =>
+            {
+                 x.Service<TickTock>(s =>
+                 {
+                     s.ConstructUsing(ticktock => new TickTock());
+                     s.WhenStarted(ticktock => ticktock.Start());
+                     s.WhenStopped(ticktock => ticktock.Stop());
+                 });
+
+                 x.RunAsLocalSystem();
+                 x.SetServiceName("TickTockService");
+                 x.SetDisplayName("Tick Tock Service");
+                 x.SetDescription("This service is created by Me: The Clock Is Ticking - Tick Tock");
+
+             });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
         }
     }
 }
---
+++ TickTockWindowsService/TickTock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Timers;
+using System.IO;
+
+namespace TickTockWindowsService
+{
+    public class TickTock
+    {
+        private readonly Timer _timer;
+        public string _file;
+
+        public TickTock()
+        {
+            System.IO.Directory.CreateDirectory("D:/Temp");
+            _file = "D:/Temp/TickTock.txt";
+
+            if (!File.Exists(_file))
+            {
+                File.Create(_file).Close();
+                /*If we will not close the file after creating it, 
+                  it cannot be used immediately to write something 
+                  here and we get the exception that it cannot be accessed 
+                  because it is being used by another process*/
+            }
+
+            // Timer interval is seeting up here
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            // Our code goes here START
+
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(_file, lines);
+
+            // ur code goes here END
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
